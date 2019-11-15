00aa5c24b648b4744cf52a6a43713ab1b2cd287e
enju <skr0tm@protonmail.com>
2019-11-15 22:32:55 +0100

    make into a go package

---
+++ README.md
@@ -0,0 +1,3 @@
+# go-collage
+
+A go package to create collages from images, like album covers
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
-package main
+package collage
 
 import (
-	"fmt"
 	"image"
 	"image/draw"
 	"image/jpeg"
@@ -27,27 +26,24 @@ func sortedKeys(m map[int]string) []int {
 	return keys
 }
 
-func mapImages(images map[int]string) map[int]*PixelImage {
+func MapImages(images map[int]string) (map[int]*PixelImage, error) {
 	files := make(map[int]*PixelImage)
 	sqr := int(math.Round(math.Sqrt(float64(len(images)))))
-	fmt.Println(len(images), math.Sqrt(float64(len(images))), float64(len(images)))
 	keys := sortedKeys(images)
 	h := 0
 	w := 0
 	for _, k := range keys {
 		files[k] = new(PixelImage)
 		path := images[k]
-		fmt.Println(k, path, h)
 		file, err := os.Open(path)
 		if err != nil {
-			fmt.Println(err)
+			return nil, err
 		}
 		files[k].img, _, err = image.Decode(file)
 		if err != nil {
-			fmt.Println(err)
+			return nil, err
 		}
 		x := k - 1
-		fmt.Println("sqr", sqr, "- x", x, "- k", k, "- %", k%sqr)
 		if k == 0 {
 			files[0].sp = image.Point{0, 0}
 		} else {
@@ -61,52 +57,35 @@ func mapImages(images map[int]string) map[int]*PixelImage {
 			h += files[x].img.Bounds().Dy()
 			w = 0
 		}
-		fmt.Println(files[k].sp, h)
 
 		files[k].rectangle = image.Rectangle{files[k].sp, files[k].sp.Add(files[k].img.Bounds().Size())}
 
 	}
-	return files
+	return files, nil
 }
 
-func main() {
-	var images = map[int]string{
-		0:  "/home/enju/pic/np.jpg",
-		1:  "/home/enju/pic/np2.jpg",
-		2:  "/home/enju/pic/np3.jpg",
-		3:  "/home/enju/pic/np4.jpg",
-		4:  "/home/enju/pic/np5.jpg",
-		5:  "/home/enju/pic/np6.jpg",
-		6:  "/home/enju/pic/np7.jpg",
-		7:  "/home/enju/pic/np8.jpg",
-		8:  "/home/enju/pic/np9.jpg",
-		9:  "/home/enju/pic/np10.jpg",
-		10: "/home/enju/pic/np11.jpg",
-		11: "/home/enju/pic/np12.jpg",
-		12: "/home/enju/pic/np13.jpg",
-		13: "/home/enju/pic/np14.jpg",
-		14: "/home/enju/pic/np15.jpg",
-		15: "/home/enju/pic/np16.jpg",
-	}
+func MakeNewCollage(images map[int]*PixelImage, output string, quality int) error {
+	// create the sizing for the collage
+	r := image.Rectangle{image.Point{0, 0}, images[len(images)-1].rectangle.Max}
 
-	files := mapImages(images)
-	fmt.Println(files)
-
-	r := image.Rectangle{image.Point{0, 0}, files[len(files)-1].rectangle.Max}
-
+	// create the new image
 	rgba := image.NewRGBA(r)
 
-	for _, PxI := range files {
+	// iterate the images and add them to the collage
+	for _, PxI := range images {
 		draw.Draw(rgba, PxI.rectangle, PxI.img, image.Point{0, 0}, draw.Src)
 	}
 
-	out, err := os.Create("./image.jpg")
+	out, err := os.Create(output)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
 	var opt jpeg.Options
-	opt.Quality = 80
+	opt.Quality = quality
 
 	jpeg.Encode(out, rgba, &opt)
+
+	return nil
+
 }
